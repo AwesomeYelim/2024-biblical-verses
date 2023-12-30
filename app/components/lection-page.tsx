07df223adc97be9fb7984a92bf7e730f4f3d2205
Awesomeyelim <uiop01900@gmail.com>
2023-12-30 16:46:49 +0900

    design

--- app/components/lection-page.tsx
+++ app/components/lection-page.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 
-import React, { useEffect, useState } from 'react'
-import randomIndex from '@/app/components/random-index'
-import Image from 'next/image'
+import React, { useEffect, useState } from "react";
+import randomIndex from "@/app/components/random-index";
+import Image from "next/image";
 
 interface Props {
-  words: { [key in string]: string }[]
+  words: { [key in string]: string }[];
 }
 
 const ImgEl = ({
@@ -13,14 +13,14 @@ const ImgEl = ({
   size,
   style,
 }: {
-  name: string
-  size: { width: number; height: number }
-  style?: React.CSSProperties & React.RefAttributes<HTMLImageElement>
+  name: string;
+  size: { width: number; height: number };
+  style?: React.CSSProperties & React.RefAttributes<HTMLImageElement>;
 }) => {
-  const extention = name.includes('.')
+  const extention = name.includes(".");
   return (
     <Image
-      className={extention ? name.split('.')[0] : name}
+      className={extention ? name.split(".")[0] : name}
       src={extention ? `/images/${name}` : `/images/${name}.svg`}
       alt={name}
       loading="eager"
@@ -28,30 +28,26 @@ const ImgEl = ({
       style={style}
       {...size}
     />
-  )
-}
-type IS = { [x: string]: string }
+  );
+};
+type IS = { [x: string]: string };
 export const LectionPage = ({ words }: Props): JSX.Element => {
-  const [modal, setModal] = useState(false)
-  const [delay, setDelay] = useState(false)
-  const target = words[randomIndex(words.length)]
-  const [lection, setLection] = useState<IS>({})
-  const images = ['kakao', 'url', 'imgdown', 'instagram']
+  const [modal, setModal] = useState(false);
+  const [delay, setDelay] = useState(false);
+  const target = words[randomIndex(words.length)];
+  const [lection, setLection] = useState<IS>({});
+  const images = ["kakao", "url", "imgdown", "instagram"];
 
   useEffect(() => {
-    if (modal) setTimeout(() => setDelay(modal), 1000)
-    else setDelay(modal)
-    setLection(target)
-  }, [modal])
+    if (modal) setTimeout(() => setDelay(modal), 1000);
+    else setDelay(modal);
+    setLection(target);
+  }, [modal]);
 
   return (
     <div className="lection_wrap">
       <div className="letter">
-        <ImgEl
-          name="letter-back"
-          size={{ width: 530, height: 132 }}
-          style={delay ? { opacity: 0 } : {}}
-        />
+        <ImgEl name="letter-back" size={{ width: 530, height: 132 }} style={delay ? { opacity: 0 } : {}} />
 
         {modal && (
           <div className="modal">
@@ -61,7 +57,7 @@ export const LectionPage = ({ words }: Props): JSX.Element => {
 
             <div className="text_area">
               <p>{lection.description}</p>
-              <p>{lection.from.replace(/[\(\)]/g, '')}</p>
+              <p>{lection.from.replace(/[\(\)]/g, "")}</p>
             </div>
             {/* <div className="sns_icon">
               {images.map((el) => {
@@ -77,17 +73,13 @@ export const LectionPage = ({ words }: Props): JSX.Element => {
             <ImgEl name="bottom_img" size={{ width: 76, height: 104 }} />
           </div>
         )}
-        <ImgEl
-          name="letter-after"
-          size={{ width: 530, height: 260 }}
-          style={delay ? { opacity: 0 } : {}}
-        />
+        <ImgEl name="letter-after" size={{ width: 530, height: 260 }} style={delay ? { opacity: 0 } : {}} />
         <ImgEl
           name="letter-cover.png"
           size={{ width: 530, height: 186 }}
           style={{
-            transformOrigin: 'top center',
-            transform: modal ? 'rotateX(-180deg)' : '',
+            transformOrigin: "top center",
+            transform: modal ? "rotateX(-180deg)" : "",
             opacity: delay ? 0 : 1,
           }}
         />
@@ -95,11 +87,10 @@ export const LectionPage = ({ words }: Props): JSX.Element => {
       <button
         className="lection-button"
         onClick={() => {
-          setModal(!modal)
-        }}
-      >
-        2024 내게 주신 하나님의 말씀
+          setModal(!modal);
+        }}>
+        {modal ? "다시 할래요 !" : "2024 내게 주신 하나님의 말씀"}
       </button>
     </div>
-  )
-}
+  );
+};
